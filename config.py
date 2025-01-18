7f428113d52ae515080a24d0d6ab760d958090b3
planc <hubenchang0515@outlook.com>
2025-01-18 14:03:20 +0800

    通过 giscus 实现评论功能

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ CONFIG = {
     "Base": "Phosphophyllite",
     "Nickname": "PlanC",
     "Username": "hubenchang0515",
+    "Repo": "hubenchang0515/Phosphophyllite",
     "Friends": {
         "Moe Tools": "https://hubenchang0515.github.io/moe-tools/",
         "QtTheme": "https://hubenchang0515.github.io/QtTheme/",
