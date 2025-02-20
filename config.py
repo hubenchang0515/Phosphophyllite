82bd64b61e55156ea634097f312c920d00f48554
Plan C <hubenchang0515@outlook.com>
2025-02-20 11:27:56 +0800

    增加友链

--- config.py
+++ config.py
@@ -4,8 +4,10 @@ CONFIG = {
     "Username": "hubenchang0515",
     "Repo": "hubenchang0515/Phosphophyllite",
     "Friends": {
+        "Moe Desktop": "https://hubenchang0515.github.io/",
         "Moe Tools": "https://hubenchang0515.github.io/moe-tools/",
         "QtTheme": "https://hubenchang0515.github.io/QtTheme/",
         "Shift": "https://hubenchang0515.github.io/shift/",
+        "更多": "https://hubenchang0515.github.io/friends/"
     },
 }
