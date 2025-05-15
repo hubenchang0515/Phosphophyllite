7db1d9868b68375455e36b6b8ce22a663b5aed11
Plan C <hubenchang0515@outlook.com>
2025-05-15 22:10:50 +0800

    布局调整

--- config.py
+++ config.py
@@ -5,10 +5,11 @@ CONFIG = {
     "Username": "hubenchang0515",
     "Repo": "hubenchang0515/Phosphophyllite",
     "Friends": {
-        "Moe Desktop": "https://hubenchang0515.github.io/",
-        "Moe Tools": "https://hubenchang0515.github.io/moe-tools/",
+        "主页": "https://hubenchang0515.github.io/",
+        "Primers 编程教程": "https://hubenchang0515.github.io/primers/",
         "QtTheme": "https://hubenchang0515.github.io/QtTheme/",
-        "Shift": "https://hubenchang0515.github.io/shift/",
-        "更多": "https://hubenchang0515.github.io/friends/"
+        "Shift 在线编程": "https://hubenchang0515.github.io/shift/",
+        "萌萌工具箱": "https://hubenchang0515.github.io/moe-tools/",
+        "友情链接": "https://hubenchang0515.github.io/friends/"
     },
 }
